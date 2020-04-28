1daa2adc3ef9641bf242a0eed69d18d3ae701d44
jhatting <jarrodhatting@icloud.com>
2020-04-27 17:54:39 -0700

    update

--- udemy/AwesomeProject/scr/generator.js
+++ udemy/AwesomeProject/scr/generator.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Button,TouchableWithoutFeedback } from 'react-native';
+
+// const generate = () => {
+//     return (
+//         <Button
+//         title="add one"
+//         onPress={() => alert('trigger')}
+//         />
+//     )
+// }
 
 const generate = () => {
-    return (
-        <View style={styles.generate}>
-            <Text style={{color:'#fff'}}>Add Number</Text>
-        </View>
-    )
+   return (
+       <TouchableWithoutFeedback
+           //onPress={() => alert('trigger')}
+           //onLongPress={() => alert('trigger')}
+           //onPressIn={() => alert('trigger')}
+           onPressOut={() => alert('trigger')}
+           >
+       <View style={styles.generate}>
+           <Text style={{color:'#fff'}}>Add Number</Text>
+       </View>
+       </TouchableWithoutFeedback>
+)
 }
 
 const styles = StyleSheet.create({
