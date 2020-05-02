46342493e46d93b9edf4c234c4522aabf3b94dde
jhatting <jarrodhatting@icloud.com>
2020-05-01 17:41:28 -0700

    update

--- Traversy_media/ShoppingList/App.js
+++ Traversy_media/ShoppingList/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    
+
+  ]);
+
   return (
     <View style={styles.container}>
     <Header  />
+    <FlatList 
+      data={items}
+      renderItem={({item}) => <ListItem/>} 
+      />
     </View>
   );
 };
